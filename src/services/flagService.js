6b291345f0f9f127de9e8ea1567c230fb9b8cc08
Adil Khan <adil_97@hotmail.it>
2026-07-09 15:56:32 +0100

    update flag service to use backend api

--- src/services/flagService.js
+++ src/services/flagService.js
@@ -2,14 +2,23 @@ const REST_URL = 'https://project-3-back-end-jutl.onrender.com/countries';
 
 const getByRegion = async (region) => {
   const res = await fetch(`${REST_URL}/region/${region}`);
-  if (!res.ok) throw new Error('Failed to fetch flags data');
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch flags data');
+  }
+
   return await res.json();
 };
 
 const getByCode = async (code) => {
   const res = await fetch(`${REST_URL}/alpha/${code}`);
-  if (!res.ok) throw new Error('Failed to fetch country detail');
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch country detail');
+  }
+
   const data = await res.json();
+
   return Array.isArray(data) ? data[0] : data;
 };
 
